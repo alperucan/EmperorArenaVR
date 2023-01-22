81c8650b0a5f93ae7cae42e056a652f1801c98a0
alperucan <74135747+alperucan@users.noreply.github.com>
2023-01-22 20:28:59 +0300

    git ignore

---
+++ Assets/Scripts/ModularCharacterController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public enum Gender
+{
+    Male,
+    Female
+    
+}
+[Serializable]
+public enum PartType
+{
+    Helmet,
+    HeadAttachment,
+    Head,
+    Hat,
+    Mask,
+    HeadCovering,
+    Hair,
+    Eyebrows,
+    Ear,
+    FacialHair,
+    BackAttachment,
+    Torso,
+    ShoulderAttachmentRight,
+    ShoulderAttachmentLeft,
+    ArmUpperRight,
+    ArmUpperLeft,
+    ElbowAttachmentRight,
+    ElbowAttachmentLeft,
+    ArmLowerRight,
+    ArmLowerLeft,
+    HandRight,
+    HandLeft,
+    HipsAttachment,
+    Hips,
+    KneeAttachmentRight,
+    KneeAttachmentLeft,
+    LegRight,
+    LegLeft
+}
+    public class ModularCharacterController : MonoBehaviour
+    {
+        
+    }
